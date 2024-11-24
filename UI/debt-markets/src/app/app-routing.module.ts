04e2c31055abd792f68ba0148d352eeb3b619ba2
fweni <fweni@student.wethinkcode.co.za>
2024-11-24 13:07:59 +0200

    added some global style

--- UI/debt-markets/src/app/app-routing.module.ts
+++ UI/debt-markets/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent
+  },
+  {
+    path: 'home',
+    component: LandingComponent
+  },
+  {
+    path: 'register',
+    component: GetStartedComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UI/debt-markets/src/app/app.module.ts
+++ UI/debt-markets/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { GetStartedComponent } from './get-started/get-started.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LandingComponent,
+    GetStartedComponent,
+    LoginComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
