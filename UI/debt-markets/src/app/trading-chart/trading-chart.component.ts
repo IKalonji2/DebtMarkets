2044c40057b4e99616128dc2bb74dcdea504edd7
fweni <fweni@student.wethinkcode.co.za>
2024-11-29 12:54:25 +0200

    trading chart component added

---
+++ UI/debt-markets/src/app/trading-chart/trading-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trading-chart',
+  templateUrl: './trading-chart.component.html',
+  styleUrl: './trading-chart.component.css'
+})
+export class TradingChartComponent {
+
+}
